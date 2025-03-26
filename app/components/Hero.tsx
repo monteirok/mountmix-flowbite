2f1ee0cc9039e0bc1b4e5018a2ce6831f97a9115
karstenmonteiro <karsten.monteiro@edu.sait.ca>
2025-03-26 04:55:27 -0600

    inital commit

---
+++ app/components/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+    return (
+        <div className="container mx-auto flex flex-col justify-center items-center min-h-screen text-center glass">
+            <h1 className="text-6xl md:text-7xl lg:text-8xl font-black">WELCOME TO MOUNTAIN MIXOLOGY</h1>
+        </div>
+    );
+}
